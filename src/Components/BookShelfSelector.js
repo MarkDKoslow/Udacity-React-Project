19a7066c6de737914505a6b28bc4d85e60e6ca3c
Mark Koslow <markdkoslow2@gmail.com>
2017-08-05 14:54:13 -0400

    refactor: Extract BookSelfSelector logic into separate component

--- src/Components/BookShelfSelector.js
+++ src/Components/BookShelfSelector.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import '../App.css'
 
+export const BookStatusEnum = {
+  CURRENTLYREADING: 'Currently Reading',
+  WANTTOREAD: 'Want To Read',
+  READ: 'Read',
+  NONE: 'None'
+}
+
 class BookShelfSelector extends Component {
   static propTypes = {
-    selectedOption: PropTypes.string.isRequired,
-    onShelfChange: PropTypes.func.isRequired
+    onShelfChange: PropTypes.func.isRequired,
+    selectedOption: PropTypes.oneOf(Object.values(BookStatusEnum)).isRequired
   }
 
   render() {
-
+    return (
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
   }
 }
 
